0c066ee9addddc880deb89639506f1e5be98651d
Ahmet ŞIK <koneviahmet@gmail.com>
2025-03-23 02:09:41 +0300

    add allias

--- copy/vue-cli/vite.config.js
+++ copy/vue-cli/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,10 @@ export default defineConfig({
     minify: 'terser',
     cssCodeSplit: true,
     sourcemap: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
   }
 })
